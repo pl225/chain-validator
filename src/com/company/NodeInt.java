5dff5bb3f0c49a15f8eac00c1c732658e9703cfd
Matheus Abreu <matheusa596@gmail.com>
2021-05-25 17:28:34 -0300

    Atualizando as classes filhas de Node.

--- src/com/company/NodeInt.java
+++ src/com/company/NodeInt.java
@@ -1,29 +1,14 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+public class NodeInt extends Node<Integer> {
 
-public class NodeInt implements Node {
-
-    private final int number;
-
-    public NodeInt (int number) {
-        this.number = number;
+    public NodeInt (Integer number) {
+        super(number);
     }
 
     @Override
-    public List<SimpleNode> getKeys() {
-        if (number <= 9) {
-            return Collections.singletonList(
-                    new SimpleNode(Objects.hashCode(this.number))
-            );
-        } else {
-            return Arrays.asList(
-                new SimpleNode(Objects.hashCode(number / 10)), // primeiro digito
-                new SimpleNode(Objects.hashCode(number % 10)) // segundo digito
-            );
-        }
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
     }
+
 }
--- src/com/company/NodeString.java
+++ src/com/company/NodeString.java
@@ -1,26 +1,14 @@
 package com.company;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-public class NodeString implements Node {
-
-    private final String word;
+public class NodeString extends Node<String> {
 
     public NodeString(String word) {
-        this.word = word;
+        super(word);
     }
 
     @Override
-    public List<SimpleNode> getKeys() {
-        if (this.word.length() == 1) {
-            return Collections.singletonList(
-                    new SimpleNode(Objects.hashCode(word.charAt(0)))
-            );
-        } else {
-            return this.word.chars().mapToObj(c -> new SimpleNode(Objects.hashCode(c))).collect(Collectors.toList());
-        }
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
     }
+
 }
